119d45fe4d6e9463b8300ecc8418161ee0581c38
Sahar <saharhalim151@gmail.com>
2023-11-03 15:26:53 -0400

    add student report file

---
+++ student_report.py
@@ -0,0 +1,32 @@
+class Student:
+  def __init__(self, name, age, gender):
+    self.name = name
+    self.age = age
+    self.gender = gender
+  
+  def student_grades(self, grade):
+    self.grade =grade
+    return grade
+
+class Course:
+  def __init__(self, name, max_students) :
+     self.name = name
+     self.max_students = max_students
+     self.students = []
+     
+  def add_student(self, student):
+    if len(self.students) < self.max_students:
+        self.students.append(student)
+        return True
+    return False
+  
+  def get_average_grade(self):
+    self.students 
+
+s1 = Student("Johnny", 19, 95)
+s2 = Student("Ben",20, 78)
+s3 = Student("Kelly", 19, 70)
+
+course = Course("Science", 2)
+course.add_student(s1)
+course.add_student(s2)
